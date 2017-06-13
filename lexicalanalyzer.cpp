7b9cab576989d3243e94407734193a46964fc53a
mahi97 <mohammadmahdi76@gmail.com>
2017-06-13 11:22:48 +0430

    add filtering for diffrent token types

--- lexicalanalyzer.cpp
+++ lexicalanalyzer.cpp
@@ -2,13 +2,102 @@
 
 LexicalAnalyzer::LexicalAnalyzer()
 {
+    keywordCnt = 0;
+    identifierCnt = 0;
+    operatorCnt = 0;
+    numberCnt = 0;
+    characterCnt = 0;
+    punctuationCnt = 0;
+
+    validKeywords << "if"
+                  << "else"
+                  << "while"
+                  << "int"
+                  << "char"
+                  << "bool"
+                  << "null"
+                  << "true"
+                  << "false";
+
 
 }
 
 void LexicalAnalyzer::analyze(QTextStream* _textStream)
 {
+    qDebug() << validKeywords;
     while (!_textStream->atEnd()) {
         QString line = _textStream->readLine();
-
+        QStringList tokens = line.split(' ');
+        for (int i = 0 ; i < tokens.size() ; i++) {
+            TokenType tempType = getTokenType(tokens.at(i));
+            if (tempType == TokenType::UNKNOWN) {
+                qDebug() << "Wrong Token : " << tokens.at(i) << " AT : " << i;
+            } else {
+                appendToTables(tempType, tokens.at(i));
+            }
+        }
     }
+}
+
+TokenType LexicalAnalyzer::getTokenType(QString _token)
+{
+    if      (validKeywords.contains(_token))    return TokenType::Keyword;
+    else if (validOperator.contains(_token))    return TokenType::Operator;
+    else if (validPunctuation.contains(_token)) return TokenType::Punctuation;
+    else if (isValidIdentifier(_token))         return TokenType::Identifier;
+    else if (isValidCharacter(_token))          return TokenType::Character;
+    else if (isValidNumber(_token))             return TokenType::Number;
+    else                                        return TokenType::UNKNOWN;
+}
+
+bool LexicalAnalyzer::isValidIdentifier(QString _token)
+{
+    QRegExp rx("∧([a−z][A−Z])+([a−z][A−Z][0−9])∗");
+    return rx.exactMatch(_token);
+}
+
+bool LexicalAnalyzer::isValidCharacter(QString _token)
+{
+    if (_token.size() == 1) return _token.at(0).isLetter();
+    else                    return false;
+}
+
+bool LexicalAnalyzer::isValidNumber(QString _token)
+{
+    if (_token.size() == 1) return _token.at(0).isNumber();
+    else                    return false;
+}
+
+void LexicalAnalyzer::appendToTables(TokenType _type, QString _token)
+{
+    switch (_type) {
+    case TokenType::Keyword :
+        symbolTable.append(qMakePair(_type, keywordCnt));
+        keywordTable.append(qMakePair(keywordCnt, getKeywordType(_token)));
+        keywordCnt++;
+        break;
+    case TokenType::Identifier :
+
+        break;
+    case TokenType::Operator :
+
+        break;
+    case TokenType::Number :
+
+        break;
+    case TokenType::Punctuation :
+
+        break;
+    case TokenType::Character :
+
+        break;
+    case TokenType::UNKNOWN:
+        qDebug() << "Something went wrong in filtering";
+        break;
+    }
+}
+
+KeywordType LexicalAnalyzer::getKeywordType(QString _str)
+{
+    return KeywordType(validKeywords.indexOf(_str));
 }
--- lexicalanalyzer.h
+++ lexicalanalyzer.h
@@ -6,6 +6,8 @@
 #include <QFile>
 #include <QList>
 #include <QPair>
+#include <QRegExp>
+#include <QDebug>
 
 enum class TokenType {
     Keyword,
@@ -13,19 +15,33 @@ enum class TokenType {
     Operator,
     Number,
     Punctuation,
-    Character
+    Character,
+    UNKNOWN
 };
 
 enum class KeywordType {
-    IF,
-    ELSE,
-    WHILE,
-    INT,
-    CHAR,
-    BOOL,
-    _NULL,
-    TRUE,
-    FALSE
+    IF    = 0,
+    ELSE  = 1,
+    WHILE = 2,
+    INT   = 3,
+    CHAR  = 4,
+    BOOL  = 5,
+    _NULL = 6,
+    TRUE  = 7,
+    FALSE = 8
+};
+
+struct IdentifierData {
+    QString value;
+    int* registerAddr;
+    int* memoryAddr;
+};
+
+struct NumberData {
+    QString name;
+    int value;
+    int* registerAddr;
+    int* memoryAddr;
 };
 
 class LexicalAnalyzer
@@ -35,14 +51,34 @@ public:
     void analyze(QTextStream *_textStream);
 
 private:
+    // methodes
+    TokenType getTokenType(QString);
+    bool isValidIdentifier(QString);
+    bool isValidCharacter(QString);
+    bool isValidNumber(QString);
+    void appendToTables(TokenType, QString);
+    KeywordType getKeywordType(QString);
+
     // Tables
     QList < QPair < TokenType, int > > symbolTable;
     QList < QPair < int, KeywordType > > keywordTable;
+    QList < QPair < int, IdentifierData > > identiTable;
+    QList < QPair < int, NumberData > > numberTable;
+    QList < QPair < int, char > > characterTable;
+    QList < QPair < int, QString > > operatorTable;
 
     // Lists
     QList < QString > validKeywords;
     QList < QString > validOperator;
     QList < QString > validPunctuation;
+
+    // symbol counters
+    int keywordCnt;
+    int identifierCnt;
+    int operatorCnt;
+    int numberCnt;
+    int characterCnt;
+    int punctuationCnt;
 
 };
 
